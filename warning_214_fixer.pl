11518e21d1dda08c13f2dd0b5862676f76eefd50
WoutProvost <wout_provost@hotmail.com>
2019-04-18 19:42:30 +0200

    Add ERROR and SUCCESS when printing

--- warning_214_fixer.pl
+++ warning_214_fixer.pl
@@ -10,7 +10,7 @@ if(@ARGV < 2) {
 
 # Check if the source file and the destination file are equal
 if($ARGV[0] eq $ARGV[1]) {
-	die "The source file can not be the same as the destination file, to prevent losing the original.\n"
+	die "ERROR: The source file can not be the same as the destination file, to prevent losing the original.\n"
 }
 
 # Declare temporary files
@@ -20,11 +20,11 @@ my $warnings_temp = "warning_214_fixer.txt";
 # Execute the compiler in a child process, redirecting stdout to trash and stderr to a temporary file
 my $devnull = File::Spec->devnull();
 if(system "pawncc $ARGV[0] -o$amx_temp > $devnull 2> $warnings_temp") {
-	die "Problem trying to execute the pawn compiler.\n";
+	die "ERROR: Problem trying to execute the pawn compiler.\n";
 }
 
 # Extract the line number and the string name from all lines containing 'warning 214' from the temporary stderr file
-open INPUT, $warnings_temp or die "Can't open temporary file '$warnings_temp': $!.\n";
+open INPUT, $warnings_temp or die "ERROR: Can't open temporary file '$warnings_temp': $!.\n";
 my %warnings;
 my $amount = 0;
 while(<INPUT>) {
@@ -37,8 +37,8 @@ while(<INPUT>) {
 close INPUT;
 
 # Add 'const' in front of every string that needs it
-open INPUT, $ARGV[0] or die "Can't open source file '$ARGV[0]': $!.\n";
-open OUTPUT, ">$ARGV[1]" or die "Can't open destination file '$ARGV[1]': $!.\n";
+open INPUT, $ARGV[0] or die "ERROR: Can't open source file '$ARGV[0]': $!.\n";
+open OUTPUT, ">$ARGV[1]" or die "ERROR: Can't open destination file '$ARGV[1]': $!.\n";
 foreach my $ln (sort {$a <=> $b} keys %warnings) {
 	# The line numbers are sorted in ascending order, so we only need to go over the source file once
 	while(<INPUT>) {
@@ -68,4 +68,4 @@ unlink $amx_temp;
 unlink $warnings_temp;
 
 # Write message
-print "Fixed warning 214 a total of $amount times.\n";
+print "SUCCESS: Fixed warning 214 a total of $amount times.\n";
